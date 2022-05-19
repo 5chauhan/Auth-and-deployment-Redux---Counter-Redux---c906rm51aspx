b83015ad66feba7ab4980dc90c20177a5f9fbc58
Sourabh Chauhan <isurabhkumar@gmail.com>
2022-05-19 17:18:19 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,20 @@
-export default (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './Components/App'
+import counter from './reducers'
 
+const store = createStore(counter)
+const rootEl = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+  <Counter
+    value={store.getState()}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
+  rootEl
+)
+
+render()
+store.subscribe(render)
