94c072160af4602fe0b820bba5a7e561d363a343
Sourabh Chauhan <isurabhkumar@gmail.com>
2022-05-19 17:16:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
-import React, { useReducer } from 'react'
-import { Reducer } from '../reducers/index';
-import '../styles/App.css';
-const App = () => {
-  const [state,dispatch] = useReducer;
-  
- 
-  return (
-    <div id="main">
-      <Counter />
-    </div>
-  )
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
+
+  render() {
+    const { value, onIncrement, onDecrement } = this.props
+    return (
+      <p >
+        Clicked: {value} times
+        {' '}
+        <button onClick={onIncrement}>
+          +
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          -
+          </button>
+      
+      </p>
+    )
+  }
 }
 
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+}
 
-export default App;
+export default Counter
