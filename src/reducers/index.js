722394ef8869df26355c76464b9aa1aad0ddde79
Sourabh Chauhan <isurabhkumar@gmail.com>
2022-05-19 17:15:08 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,11 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
-class Counter extends React.Component {
-  increment = () => {
-    this.props.dispatch({ type: 'INCREMENT' });
-  }
-
-  decrement = () => {
-    this.props.dispatch({ type: 'DECREMENT' });
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Counter</h2>
-        <div>
-          <button onClick={this.decrement}>-</button>
-          <span data-testid='counter'>{this.props.count}</span>
-          <button onClick={this.increment}>+</button>
-        </div>
-      </div>
-    )
+export default (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    count: state.count
-  };
-}
-
-export default connect(mapStateToProps)(Counter);
